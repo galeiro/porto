5366a98d9db0ccf0828d1f6f0e7b545d3c056414
Galeiro <galihsaputra1234@gmail.com>
2023-10-11 15:15:26 +0700

    ubah: navigasi

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex items-center justify-center gap-4">
-					{navigation.map((item) => (
+					{/* {navigation.map((item) => (
 						<Link
 							key={item.href}
 							href={item.href}
@@ -20,7 +20,7 @@ export default function Home() {
 						>
 							{item.name}
 						</Link>
-					))}
+					))} */}
 				</ul>
 			</nav>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
