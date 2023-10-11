055ae7e69d8c9bbc6957d6d28bddd4f0980491a7
Galeiro <galihsaputra1234@gmail.com>
2023-10-11 15:30:49 +0700

    ubah: keterangan

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Sorry. this web's developer is main tenis.
+					Sorry. this web is still main tenis.
 					{/* solutions at{" "}
 					<Link
 						target="_blank"
